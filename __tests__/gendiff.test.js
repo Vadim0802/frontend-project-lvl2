ffdbad60f3c75202364891a7c013636f4d144e85
Vadim Bodianskii <v.bodyansky@yandex.ru>
2021-03-08 10:29:40 +1000

    plain format tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,14 +8,22 @@ const __dirname = path.dirname(__filename);
 const pathToFixtures = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(pathToFixtures(filename), 'utf-8');
 
-test('genDiff json', () => {
+test('genDiff stylish format', () => {
   const expected = readFile('expected_file.txt');
-  const diff = genDiff(pathToFixtures('file1.json'), pathToFixtures('file2.json'));
-  expect(diff).toBe(expected);
+
+  const diffJson = genDiff(pathToFixtures('file1.json'), pathToFixtures('file2.json'), 'stylish');
+  const diffYml = genDiff(pathToFixtures('file1.yml'), pathToFixtures('file2.yml'), 'stylish');
+
+  expect(diffJson).toBe(expected);
+  expect(diffYml).toBe(expected);
 });
 
-test('genDiff yml', () => {
-  const expected = readFile('expected_file.txt');
-  const diff = genDiff(pathToFixtures('file1.yml'), pathToFixtures('file2.yml'));
-  expect(diff).toBe(expected);
+test('genDiff plain format', () => {
+  const expected = readFile('expected_file_plain.txt');
+
+  const diffJson = genDiff(pathToFixtures('file1.json'), pathToFixtures('file2.json'), 'plain');
+  const diffYml = genDiff(pathToFixtures('file1.yml'), pathToFixtures('file2.yml'), 'plain');
+
+  expect(diffJson).toBe(expected);
+  expect(diffYml).toBe(expected);
 });
